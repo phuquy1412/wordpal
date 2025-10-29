efeb96177f5e005aed236d3476fc6be031872d0e
quyh2 <hophuquy.work@gmail.com>
2025-10-29 21:46:39 +0700

    tao giao dien

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './index.css';
+import QuizletHomePage from './HomePage'; 
+import LoginPage from './LoginPage';
+import RegisterPage from "./DangKy"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 3. Render ứng dụng
+root.render(
+  <React.StrictMode>
+   
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<RegisterPage />} />
+        {}
+        <Route path="/" element={<QuizletHomePage />} />
+        
+        {}
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
